dbaafe706af74cf173d9346b5b067f4dc3247249
zorluozan <61868614+zorluozan@users.noreply.github.com>
2023-09-06 22:49:08 +0300

    Add new components

--- src/components/CountryCard.tsx
+++ src/components/CountryCard.tsx
@@ -1,3 +1,30 @@
-export default function CountryCard() {
-  return <div>CountryCard</div>;
+import { ICountryType } from "../types/country";
+
+export default function CountryCard({ country }: ICountryType) {
+  return (
+    <article className="bg-white shadow-sm basis-72 flex-grow">
+      <img
+        src={country["flags"]?.png}
+        alt={country["flags"]?.alt}
+        className="w-full h-52"
+      />
+      <div className="p-4">
+        <p className="text-black font-bold mb-2 text-xl">
+          {country["name"]?.common}
+        </p>
+        <div className="flex items-center mb-2">
+          <p className="text-black font-semibold">Population:</p>
+          <p className="text-darkBlue font-medium">{country?.population}</p>
+        </div>
+        <div className="flex items-center mb-2">
+          <p className="text-black font-semibold">Region:</p>
+          <p className="text-darkBlue font-medium">{country?.region}</p>
+        </div>
+        <div className="flex items-center">
+          <p className="text-black font-semibold">Capital:</p>
+          <p className="text-darkBlue font-medium">{country.capital}</p>
+        </div>
+      </div>
+    </article>
+  );
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,7 @@
 import { useQuery } from "react-query";
 import axios from "axios";
 import { useEffect } from "react";
+import CountryCard from "../components/CountryCard";
 
 export default function Home() {
   async function fetchApi() {
@@ -17,39 +18,7 @@ export default function Home() {
     <div className="flex flex-wrap gap-8 bg-lightGray p-10">
       {Object.keys(data || {}).map((key) => {
         const country = data[key];
-        return (
-          <article
-            className="bg-white shadow-sm basis-72 flex-grow"
-            key={country["name"]?.common}
-          >
-            <img
-              src={country["flags"]?.png}
-              alt={country["flags"]?.alt}
-              className="w-full h-52"
-            />
-            <div className="p-4">
-              <p className="text-black font-bold mb-2 text-xl">
-                {country["name"]?.common}
-              </p>
-              <div className="flex items-center mb-2">
-                <p className="text-black font-semibold">Population:</p>
-                <p className="text-darkBlue font-medium">
-                  {country?.population}
-                </p>
-              </div>
-              <div className="flex items-center mb-2">
-                <p className="text-black font-semibold">Region:</p>
-                <p className="text-darkBlue font-medium">{country?.region}</p>
-              </div>
-              <div className="flex items-center">
-                <p className="text-black font-semibold">Capital:</p>
-                <p className="text-darkBlue font-medium">
-                  {country.capital?.at(0)}
-                </p>
-              </div>
-            </div>
-          </article>
-        );
+        return <CountryCard country={country} key={country["name"].official} />;
       })}
     </div>
   );
